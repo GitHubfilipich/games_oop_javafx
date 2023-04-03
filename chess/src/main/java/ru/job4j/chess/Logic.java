9315ebbd67383f5cb66a2d9614f57dadb685aa9b
YuryGe <postbox177@yandex.ru>
2023-04-03 09:40:40 +0400

    3. Каркас шахматной доски [#792] - исправление

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -22,10 +22,11 @@ public final class Logic {
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Cell cell : steps) {
-            try {
-            int index = findBy(cell);
-                throw new OccupiedCellException();
-            } catch (FigureNotFoundException e) { }
+            for (Figure figure: figures) {
+                if (figure != null && figure.position().equals(cell)) {
+                    throw new OccupiedCellException();
+                }
+            }
         }
         return true;
     }
